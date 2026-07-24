3e56e6ef94fb196a71f99bd657b23a90903de6cb
Orbylabs <abidinkaratay_att@hotmail.com>
2026-07-24 14:37:11 +0300

    Update page.tsx
    Add Story section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/sections/Hero";
+import Story from "@/components/sections/Story";
 import Footer from "@/components/layout/Footer";
 
 export default function HomePage() {
@@ -7,6 +8,7 @@ export default function HomePage() {
     <>
       <Navbar />
       <Hero />
+      <Story />
       <Footer />
     </>
   );
