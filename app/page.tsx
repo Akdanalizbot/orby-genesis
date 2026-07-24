a73575acbe8b5f8cfd17bf8499f8601332c623a2
Orbylabs <abidinkaratay_att@hotmail.com>
2026-07-24 14:42:26 +0300

    Update page.tsx
    Add Vision section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/sections/Hero";
 import Story from "@/components/sections/Story";
+import Vision from "@/components/sections/Vision";
 import Footer from "@/components/layout/Footer";
 
 export default function HomePage() {
@@ -9,6 +10,7 @@ export default function HomePage() {
       <Navbar />
       <Hero />
       <Story />
+      <Vision />
       <Footer />
     </>
   );
