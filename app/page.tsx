f74bede811bd4e1e2585ca40bd26968ae13e88cb
Orbylabs <abidinkaratay_att@hotmail.com>
2026-07-24 14:46:10 +0300

    Update page.tsx
    Add Universe section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/sections/Hero";
 import Story from "@/components/sections/Story";
 import Vision from "@/components/sections/Vision";
+import Universe from "@/components/sections/Universe";
 import Footer from "@/components/layout/Footer";
 
 export default function HomePage() {
@@ -11,6 +12,7 @@ export default function HomePage() {
       <Hero />
       <Story />
       <Vision />
+      <Universe />
       <Footer />
     </>
   );
