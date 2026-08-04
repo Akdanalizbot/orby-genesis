4df47b3af261cf54fe57e9c61511cb65b7b5a875
Orbyz-genesis <abidinkaratay_att@hotmail.com>
2026-08-05 00:28:30 +0300

    Update HeroLogo.tsx

--- components/hero/HeroLogo.tsx
+++ components/hero/HeroLogo.tsx
@@ -1,6 +1,6 @@
 export default function HeroLogo() {
   return (
-    <h1 className="text-7xl md:text-9xl font-black tracking-[0.45em] text-white">
+    <h1 className="text-5xl sm:text-6xl md:text-9xl font-black tracking-[0.20em] sm:tracking-[0.30em] md:tracking-[0.45em] text-white">
       MORDYZ
     </h1>
   );
